f39eac58303d1e6b12e50c0a7f165afa715a68c5
huhn511 <sehe89@gmail.com>
2020-09-22 01:09:26 +0200

    add ipfs server and wasm bridge

--- bootstrap.js
+++ bootstrap.js
@@ -1,5 +1,12 @@
 import './static/reset.scss';
 import './static/theme.scss';
+import "./js/wasm_bridge.js";
+
+
+import("./js/ipfs_server.js").then(module => {
+  //module.start_ipfs();
+});
+
 
 import("./pkg").then(module => {
   module.run_app();
---
+++ js/wasm_bridge.js
@@ -0,0 +1,3 @@
+export function js_test() {
+    console.log("hello world bridge!")
+};
