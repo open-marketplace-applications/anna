da4b27980ffd4f9d984f6d29223fca70240bf16c
huhn511 <sehe89@gmail.com>
2020-09-22 12:49:07 +0200

    add scanner page

--- src/design_system/header/header.rs
+++ src/design_system/header/header.rs
@@ -52,6 +52,9 @@ impl Component for Header {
                     <RouterAnchor<AppRoutes> route=AppRoutes::Products>
                         { "Products" }
                     </RouterAnchor<AppRoutes>>
+                    <RouterAnchor<AppRoutes> route=AppRoutes::Scanner>
+                        { "Scanner" }
+                    </RouterAnchor<AppRoutes>>
                     <RouterAnchor<AppRoutes> route=AppRoutes::Profile>
                         { "Profile" }
                     </RouterAnchor<AppRoutes>>
