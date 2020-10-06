a4a084b1b344ccfa1309d9645603f8ce3d8118ba
Sven Reiser <36033186+rep0x@users.noreply.github.com>
2020-10-06 22:43:25 +0200

    Feature/12 extract static css files into design (#13)
    * removes static scss files
    
    * add theme atom to design system and wrap it around the app
    
    * adds font as html link instead of css import (didnt work with css-in-rust)

--- bootstrap.js
+++ bootstrap.js
@@ -1,16 +1,13 @@
-import './anna_app/static/reset.scss';
-import './anna_app/static/theme.scss';
-import "./anna_app/js/wasm_bridge.js";
-import { init_scanner } from "./anna_app/js/scanner.js";
+import './anna_app/js/wasm_bridge.js'
+import { init_scanner } from './anna_app/js/scanner.js'
 
-import("./anna_app/js/ipfs.js").then(module => {
+import('./anna_app/js/ipfs.js').then((module) => {
   // module.start_ipfs();
-});
+})
 
-
-import("./anna_app/pkg").then(wasm => {
-  wasm.run_app();
-  console.log("wasm", wasm)
-  console.log("init_scanner", init_scanner)
+import('./anna_app/pkg').then((wasm) => {
+  wasm.run_app()
+  console.log('wasm', wasm)
+  console.log('init_scanner', init_scanner)
   init_scanner(wasm)
-});
+})
